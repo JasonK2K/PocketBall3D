08f9fd38a353f845dd8a313dd02cb7ca4e148365
JasonK2K <83547121+JasonK2K@users.noreply.github.com>
2022-01-19 19:46:59 +0900

    1/19

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,9 +8,19 @@ namespace JK
     public class BallMovement : MonoBehaviour
     {
         Rigidbody rb;
+        
         public Vector3 ballVelocity;
-        int BallNum;
         public Vector3 ballDirection;
+        public Vector3 powerPosition;
+
+        private float power;
+        private float start_time;
+        private float bar_time;
+        private float press_time;
+        private float end_time;
+
+        int BallNum;
+        
         //private bool trigger = true;
 
         // Start is called before the first frame update
@@ -18,6 +28,7 @@ namespace JK
         {
             rb=GetComponent<Rigidbody>();
             BallNum=int.Parse(gameObject.name.Substring(5));
+            power=0;
         }
 
         // Update is called once per frame
@@ -32,7 +43,28 @@ namespace JK
             }
         }
 
-        void OnMouseDown() //이거 흰공에만 적용해야함
+        void OnMouseDown()
+        {
+            power=0;
+            if(BallNum==0) //흰공일때
+            {
+                start_time = Time.time;
+            }
+        }
+
+        void OnMouseDrag()
+        {
+            bar_time = Time.time;
+            press_time=bar_time-start_time;
+            if(press_time>2)
+            {
+                press_time=2;
+            }
+            PowerBarScript.bar_width = (float)143.5*(press_time);
+        }
+
+
+        void OnMouseUp() //마우스를 눌렀다가 뗐을 때
         {
             
             Debug.Log(GameManager.isBallStop.Sum()); // 공이 없어지면서 뭔가 문제가 생긴듯
@@ -43,9 +75,22 @@ namespace JK
                     //흰공 방향 결정하자.
                     ballDirection=transform.position-PlayerScript.playerPosition;
                     ballDirection.y=0;                
-                    Debug.Log(PlayerScript.playerPosition.x.ToString()+','+PlayerScript.playerPosition.z.ToString());
+                    
+                    //시간 받아오기
+                    end_time = Time.time;
+                    Debug.Log(end_time - start_time);
+                    
+                    //힘 크기 결정 (500*누른 시간)
+                    power=500*(end_time - start_time);
+
+                    //최대 힘 1000
+                    if(power>1000)
+                    {
+                        power=1000;
+                    }
+
                     //힘 가함 -> 누를 수록 늘어나도록 하자.
-                    rb.AddForce(ballDirection*500);
+                    rb.AddForce(ballDirection*power);
 
                     //초기화
                     for(int i=0; i<16; i++)
@@ -53,10 +98,12 @@ namespace JK
                         GameManager.isBallStop[i]=0;
                     }
                     
-                    foreach (var human in GameManager.isBallStop)
+                    //이거 isBallStop 버그 수정할 때 사용
+                    /*foreach (var human in GameManager.isBallStop)
                     {
-                        //Debug.Log(human);
-                    }
+                        Debug.Log(human);
+                    }*/
+                    
                     //boolean 모두 true로 바꾸자
                     for(int j=0; j<16; j++)
                     {
